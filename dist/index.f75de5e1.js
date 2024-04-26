43dcbea0f2e591ba8a4195961088a171e9d20506
Lucas <lucas.flixzin@gmail.com>
2024-04-26 13:26:56 -0300

    Finalização do projeto

---
+++ dist/index.f75de5e1.js
@@ -0,0 +1,26 @@
+AOS.init();
+const dataDoEvento = new Date("Sep 26, 2024 15:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+    const diasEmMs = 86400000;
+    const horasEmMs = 3600000;
+    const minutosEmMs = 60000;
+    const diasEvento = Math.floor(distanciaAteEvento / diasEmMs);
+    const horasEvento = Math.floor(distanciaAteEvento % diasEmMs / horasEmMs);
+    const minutosEvento = Math.floor(distanciaAteEvento % horasEmMs / minutosEmMs);
+    const segundosEvento = Math.floor(distanciaAteEvento % minutosEmMs / 1000);
+    console.log(diasEvento);
+    console.log(horasEvento);
+    console.log(minutosEvento);
+    console.log(segundosEvento);
+    document.getElementById("contador").innerHTML = `${diasEvento}d ${horasEvento}h ${minutosEvento}m ${segundosEvento}s`;
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaHoras);
+        document.getElementById("contador".innerHTML = `So no ano que vem agora `);
+    }
+}, 1000);
+
+//# sourceMappingURL=index.f75de5e1.js.map
